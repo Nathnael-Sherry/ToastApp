70d2f4bb566c5248fc6a3fdc0e8a36d2d6f6e724
SalomeNderu <sallienderu@gmail.com>
2023-04-04 10:08:19 +0300

    ToastingApp-Nate

--- app/src/main/java/com/example/toastingapp_nate/MainActivity.kt
+++ app/src/main/java/com/example/toastingapp_nate/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var display_text:TextView
     private lateinit var edt_input_data:EditText
     private lateinit var submitbutton:Button
+    private lateinit var buttontwo:Button
+    private lateinit var buttonthree:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,17 @@ class MainActivity : AppCompatActivity() {
         display_text = findViewById(R.id.txtdisplay)
         edt_input_data = findViewById(R.id.edtdata)
         submitbutton = findViewById(R.id.btnsubmit)
+        buttontwo = findViewById(R.id.btnsecond)
+        buttonthree = findViewById(R.id.btnthird)
 
         submitbutton.setOnClickListener {
             Toast.makeText(this, "Welcome To My First Toast Application", Toast.LENGTH_SHORT).show()
+        }
+        buttontwo.setOnClickListener {
+            Toast.makeText(this, "Clicked the second button", Toast.LENGTH_SHORT).show()
+        }
+        buttonthree.setOnClickListener {
+            Toast.makeText(this, "Clicked the third button", Toast.LENGTH_SHORT).show()
         }
     }
 }
